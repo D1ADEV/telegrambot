73cba765425adeb3154f451872a7cdf42da9f7e5
d1adev <nicolasmanichon@yahoo.fr>
2016-06-12 01:21:12 +0200

    Added foss.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var tgapi = require("telegram-node-bot");
 var spelling = require('./spellchecking.js').SPELLCHECKER;
 var talker = require('./talking.js').TALKER;
+var foss = require('./foss.js').FOSS;
 
 var _bot = {
     start: function(token) {
@@ -9,6 +10,7 @@ var _bot = {
 
         tg.router
             .when('/talkdirtytome', 'TalkController')
+            .when('/foss', 'FossController')
             .otherwise('SpellingController');
 
         tg.controller('TalkController', ($) => {
@@ -16,37 +18,45 @@ var _bot = {
             $.sendMessage(talker.getMessage());
         });
 
+        tg.controller('FossController', ($) => {
+            console.log("Foss");
+            $.sendMessage(foss.getMessage(), {parse_mode: "html"});
+        });
+
         tg.controller('SpellingController', ($) => {
-            if ($.message.text.split(" ").length >= 3) {
-                spelling.check($.message.text, function(resp) {
-                    if (resp.toLowerCase() != $.message.text.toLowerCase()) {
-                        console.log("Correcting an asshole");
-                        $.sendMessage(
-                            "@" +
-                            $.message.from.username +
-                            " Did you mean : " +
-                            resp +
-                            " ?", {
-                                parse_mode: "html"
+            if ($.message.text != undefined) {
+                if ($.message.text.split(" ").length >= 3) {
+                    spelling.check($.message.text, function(resp) {
+                        if (resp.toLowerCase() != $.message.text.toLowerCase()) {
+                            console.log("Correcting an asshole");
+                            $.sendMessage(
+                                "@" +
+                                $.message.from.username +
+                                " Did you mean : " +
+                                resp +
+                                " ?", {
+                                    parse_mode: "html"
+                                });
+                            $.waitForRequest(($) => {
+                                if ($.message.text.toLowerCase().indexOf("yes") > -1) {
+                                    $.sendMessage("Pleasure's all mine");
+                                } else if ($.message.text.toLowerCase().indexOf("no") > -1) {
+                                    $.sendMessage("Well fuck you, asshole");
+                                }
                             });
-                        $.waitForRequest(($) => {
-                            if ($.message.text.toLowerCase().indexOf("yes") > -1) {
-                                $.sendMessage("Pleasure's all mine");
-                            } else if ($.message.text.toLowerCase().indexOf("no") > -1) {
-                                $.sendMessage("Well fuck you, asshole");
-                            }
-                        });
-                        setTimeout(function() {
-                            /* 
-                                If tg.waitingCallbacks isn't cleaned, the next message of the 
-                                user will not be spell checked. Cleaning it after 5 secs seems to be ok. 
-                            */
-                            console.log('Cleaning waiting callbacks');
-                            tg.waitingCallbacks = {};
-                        }, 5000);
-                    }
-                });
+                            setTimeout(function() {
+                                /* 
+                                    If tg.waitingCallbacks isn't cleaned, the next message of the 
+                                    user will not be spell checked. Cleaning it after 5 secs seems to be ok. 
+                                */
+                                console.log('Cleaning waiting callbacks');
+                                tg.waitingCallbacks = {};
+                            }, 5000);
+                        }
+                    });
+                }
             }
+
         });
     }
 };
---
+++ foss.js
@@ -0,0 +1,7 @@
+var _foss = {
+    getMessage: function(){
+        return 'Of course : <a href="https://github.com/d1adev/telegrambot">Github</a>';
+    }
+};
+
+exports.FOSS = _foss;
